52730e8ad2bedd503f1c3989617217e5589fffd0
taeyeongKims <xodud8530@gmail.com>
2025-06-13 15:49:55 +0900

    [feature] 소셜 로그인 기능 구현

---
+++ src/main/java/fresco/com/auth/dto/response/NaverResponse.java
@@ -0,0 +1,29 @@
+package fresco.com.auth.dto.response;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Map;
+
+public record NaverResponse(@NotNull Map<String, Object> attributes) implements OAuth2Response {
+
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attributes.get("email").toString();
+    }
+
+    @Override
+    public String getName() {
+        return attributes.get("name").toString();
+    }
+}
+
