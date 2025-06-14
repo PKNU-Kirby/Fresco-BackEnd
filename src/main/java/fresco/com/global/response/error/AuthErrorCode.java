acf6973015b4b942a27f0810050882a816435338
taeyeongKims <xodud8530@gmail.com>
2025-06-14 13:27:24 +0900

    #3 [feat] 리프레시 토큰 재발급 및 로그아웃 api 구현

--- src/main/java/fresco/com/global/response/error/AuthErrorCode.java
+++ src/main/java/fresco/com/global/response/error/AuthErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode{
-    NULL_MEMBER("AUTH_001", HttpStatus.BAD_REQUEST, "사용자가 존재하지 않습니다."),
-    INVALID_TOKEN("AUTH_002", HttpStatus.BAD_REQUEST, "올바르지 않은 액세스 토큰입니다."),
-    EXPIRED_ACCESS_TOKEN("AUTH_003", HttpStatus.UNAUTHORIZED, "만료된 액세스 토큰입니다."),
+    NULL_USER("AUTH_001", HttpStatus.BAD_REQUEST, "사용자가 존재하지 않습니다."),
+    INVALID_TOKEN("AUTH_002", HttpStatus.BAD_REQUEST, "올바르지 않은 토큰입니다."),
+    EXPIRED_TOKEN("AUTH_003", HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
     INVALID_REDIRECT_URI("AUTH_004", HttpStatus.BAD_REQUEST, "유효하지 않은 리다이렉트 경로입니다.");
 
     private final String developCode;
