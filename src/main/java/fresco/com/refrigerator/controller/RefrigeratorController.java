a5564908dfca687038a4c23260c43ae6206ac33d
taeyeongKims <xodud8530@gmail.com>
2025-06-17 14:47:57 +0900

    [feature/#10] ] 냉장고 조회, 생성, 삭제,  변경 api 구현

---
+++ src/main/java/fresco/com/refrigerator/controller/RefrigeratorController.java
@@ -0,0 +1,51 @@
+package fresco.com.refrigerator.controller;
+
+import fresco.com.global.response.SuccessResponse;
+import fresco.com.global.response.success.RefrigeratorSuccessCode;
+import fresco.com.refrigerator.controller.dto.request.*;
+import fresco.com.refrigerator.controller.dto.response.RefrigeratorInfoResponse;
+import fresco.com.refrigerator.service.RefrigeratorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/refrigerator")
+@RequiredArgsConstructor
+public class RefrigeratorController {
+    private final RefrigeratorService refrigeratorService;
+
+    @PostMapping
+    public SuccessResponse<RefrigeratorInfoResponse> createRefrigerator(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody RefrigeratorNameRequest request) {
+        return SuccessResponse.of(RefrigeratorSuccessCode.REFRIGERATOR_CREATE_SUCCESS,
+                refrigeratorService.createRefrigerator(new CreateRefrigeratorRequest(userId, request.name())));
+    }
+
+    @DeleteMapping("/{refrigeratorId}")
+    public SuccessResponse<String> deleteRefrigerator(
+            @PathVariable Long refrigeratorId) {
+        return SuccessResponse.of(RefrigeratorSuccessCode.REFRIGERATOR_DELETE_SUCCESS,
+                refrigeratorService.deleteRefrigerator(new DeleteRefrigeratorRequest(refrigeratorId)));
+    }
+
+    @PutMapping("/{refrigeratorId}")
+    public SuccessResponse<RefrigeratorInfoResponse> updateRefrigerator(
+            @PathVariable Long refrigeratorId,
+            @Valid @RequestBody RefrigeratorNameRequest request) {
+        return SuccessResponse.of(RefrigeratorSuccessCode.REFRIGERATOR_UPDATE_SUCCESS,
+                refrigeratorService.updateRefrigerator(new UpdateRefrigeratorRequest(refrigeratorId, request.name())));
+    }
+
+    @GetMapping
+    public SuccessResponse<List<RefrigeratorInfoResponse>> getAllRefrigerator(
+            @AuthenticationPrincipal Long userId
+    ) {
+        return SuccessResponse.of(RefrigeratorSuccessCode.REFRIGERATOR_LIST_SUCCESS,
+                refrigeratorService.getAllRefrigerator(new GetAllRefrigeratorRequest(userId)));
+    }
+}
---
+++ src/main/java/fresco/com/refrigerator/controller/dto/response/RefrigeratorInfoResponse.java
@@ -0,0 +1,12 @@
+package fresco.com.refrigerator.controller.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RefrigeratorInfoResponse(
+        @NotNull(message = "냉장고 id는 null이면 안됩니다.")
+        Long id,
+        @NotBlank(message = "냉장고 이름은 빈 문자열이면 안됩니다.")
+        String name
+) {
+}
