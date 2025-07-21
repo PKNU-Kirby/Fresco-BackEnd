e01316efdde04a9168c0196991174d8c3250948a
taeyeongkims <xodud8530@gmail.com>
2025-07-21 23:06:28 +0900

    .

--- src/main/java/com/example/fresco/health/HealthController.java
+++ src/main/java/com/example/fresco/health/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/hello")
+    @GetMapping("/health")
     public String helloCheck() {
         return "I am healthy";
     }
