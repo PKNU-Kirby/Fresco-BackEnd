c839727b8b1596d3500adbc2cebbc967cd6ae391
taeyeongkims <xodud8530@gmail.com>
2025-08-06 16:18:25 +0900

    [feature] 테스트 사용자 데이터 저장 및 api 구현

---
+++ src/main/java/com/example/fresco/user/test/TestController.java
@@ -0,0 +1,20 @@
+package com.example.fresco.user.test;
+
+import com.example.fresco.auth.controller.dto.request.UserIdRequest;
+import com.example.fresco.auth.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/login/test")
+@RequiredArgsConstructor
+public class TestController {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping
+    public String getTestUserAccessToken() {
+        return jwtTokenProvider.generateAccessToken(new UserIdRequest(1L));
+    }
+}
