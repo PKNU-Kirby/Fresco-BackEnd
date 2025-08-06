87636bd6ca8428e744aec048e8ca48c0fa84b2e2
taeyeongkims <xodud8530@gmail.com>
2025-08-06 16:40:51 +0900

    인증 필터 걸리지 않게 수정

--- src/main/java/com/example/fresco/user/test/TestController.java
+++ src/main/java/com/example/fresco/user/test/TestController.java
@@ -2,6 +2,10 @@ package com.example.fresco.user.test;
 
 import com.example.fresco.auth.controller.dto.request.UserIdRequest;
 import com.example.fresco.auth.util.JwtTokenProvider;
+import com.example.fresco.global.exception.RestApiException;
+import com.example.fresco.global.response.error.AuthErrorCode;
+import com.example.fresco.user.domain.User;
+import com.example.fresco.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
